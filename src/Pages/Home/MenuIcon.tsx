694bc7ed6adec6ce82b199b3018f5b08d196c89a
hanalen <jinsub2001@naver.com>
2024-04-30 16:46:56 +0900

    아이콘 방식 변경

--- src/Pages/Home/MenuIcon.tsx
+++ src/Pages/Home/MenuIcon.tsx
@@ -3,18 +3,18 @@ import { BskyAgent } from '@atproto/api';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { uiSlice } from '@/store/UISlice';
-import { SvgIcon } from '@mui/material';
+import { SvgIcon, Icon, IconButton, Box } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 
 export interface MenuIconProp {
-  icon: typeof SvgIcon;
+  icon: string;
 }
 
 function MenuIcon(prop: MenuIconProp) {
   return (
     <div className="w-10 h-10 flex justify-center cursor-pointer mb-1">
       <div className="flex flex-col justify-center">
-        <SvgIcon component={prop.icon} />
+        <Icon>{prop.icon}</Icon>
       </div>
     </div>
   );
