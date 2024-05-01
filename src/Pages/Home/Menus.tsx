981e296296ca3ee5f52158bb8ee345f67d8aca4c
hanalen <jinsub2001@naver.com>
2024-05-01 18:09:46 +0900

    좌측 메뉴 프로필사진 추가

--- src/Pages/Home/Menus.tsx
+++ src/Pages/Home/Menus.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import MenuIcon from './MenuIcon';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/Store';
@@ -6,11 +6,32 @@ import { useCommon } from '../../Providers/CommonProvider';
 
 function Menus() {
   const common = useCommon();
-
+  const { agent, agents } = useSelector((state: RootState) => state.userState);
   const { tabs } = useSelector((state: RootState) => state.uiState);
+
+  const [avatar, setAvatar] = useState('');
+
+  const LoadProfile = async () => {
+    if (!agent.session) return;
+    const profile = await agent.getProfile({ actor: agent.session.did });
+
+    if (profile.data.avatar) {
+      setAvatar(profile.data.avatar);
+    }
+  };
+
+  useEffect(() => {
+    LoadProfile();
+  }, [agent.session]);
+
   return (
     <div className="flex flex-col h-screen w-10 bg-slate-200">
-      <div>프로필아이콘</div>
+      <div className="w-8 h-8 m-1">
+        {avatar && (
+          <img src={avatar} alt="avatar" className="w-full h-full rounded-md" />
+        )}
+      </div>
+
       {tabs.map((tab, index) => {
         return <MenuIcon icon={common.GetIconName(tab.tabType)} key={index} />;
       })}
