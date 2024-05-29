32c82a7e5be37b6638ab7b6660c0807f87b752bc
hanalen <jinsub2001@naver.com>
2024-05-29 13:37:21 +0900

    Record에 $type 정의 추가

--- src/Interfaces/Record.ts
+++ src/Interfaces/Record.ts
@@ -1,8 +1,21 @@
+export enum ERecordType {
+  follow = 'app.bsky.graph.follow',
+  post = 'app.bsky.feed.post',
+  like = 'app.bsky.feed.like',
+  quote = 'app.bsky.graph.quote',
+}
+
 export interface Record {
-  $type: string;
+  $type: ERecordType;
   createdAt: string;
   facets: any[];
   langs: string[];
   text: string;
   via: string;
+  subject: Subject;
+}
+
+export interface Subject {
+  cid: string;
+  uri: string;
 }
